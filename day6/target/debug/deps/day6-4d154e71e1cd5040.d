194d485f977f25939cc6ab87331311a32d3f1750
Neelzee <nilsien2001@gmail.com>
2023-12-06 10:02:32 +0100

    rust init

---
+++ day6/target/debug/deps/day6-4d154e71e1cd5040.d
@@ -0,0 +1,5 @@
+C:\Users\nilsi\AoC2023\day6\target\debug\deps\libday6-4d154e71e1cd5040.rmeta: src\main.rs
+
+C:\Users\nilsi\AoC2023\day6\target\debug\deps\day6-4d154e71e1cd5040.d: src\main.rs
+
+src\main.rs:
---
+++ day6/target/debug/deps/day6-52fc5221b2e89c26.d
@@ -0,0 +1,5 @@
+C:\Users\nilsi\AoC2023\day6\target\debug\deps\libday6-52fc5221b2e89c26.rmeta: src\main.rs
+
+C:\Users\nilsi\AoC2023\day6\target\debug\deps\day6-52fc5221b2e89c26.d: src\main.rs
+
+src\main.rs:
